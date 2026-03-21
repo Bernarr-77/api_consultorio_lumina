e364fafd6df877a8cd1fd2050a09b30916530c98
Bernarr-77 <Bernarrnarciso@outlook.com>
2026-03-21 13:11:37 -0300

    Tabela provider criada com chave estrangeira

--- app/db/models.py
+++ app/db/models.py
@@ -1,6 +1,6 @@
 from app.db.session import Base
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Enum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Enum, ForeignKey
 import enum
 from datetime import datetime
 class StatusUsuario(enum.Enum):
@@ -15,3 +15,12 @@ class User(Base):
     hashed_password: Mapped[str] = mapped_column(nullable=False)
     role: Mapped[str] = mapped_column(Enum(StatusUsuario),nullable=False)
     created_at:Mapped[datetime] = mapped_column(default=datetime.now)
+    provider: Mapped["Provider"] = relationship(back_populates="user")
+
+class Provider(Base):
+    __tablename__ = "providers"
+    id: Mapped[int] = mapped_column(primary_key= True, autoincrement= True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("usuarios.id"))
+    bio: Mapped[str] = mapped_column(nullable=False)
+    specialty: Mapped[str] = mapped_column(nullable=False)
+    user: Mapped["User"]= relationship(back_populates="provider")
